f26b6f84f871d2f804e63a10221baa673a044a1d
TNieAccStudy <tnieonlyu1706@gmail.com>
2026-05-14 10:45:13 +0700

    feat: add sfx for base gameplay

---
+++ Assets/_Project/Scripts/Gameplay/BaseGameplay/BaseObject/Configurators/InteractSfxConfigurator.cs
@@ -0,0 +1,39 @@
+using _Project.Scripts.Gameplay.Global.SoundSystem;
+using Reflex.Attributes;
+using Reflex.Extensions;
+using SoundSystem.Core;
+using UnityEngine;
+
+// Thay đổi theo namespace thực tế của Reflex nếu cần
+
+namespace Game.BaseGameplay.Configurators
+{
+    [CreateAssetMenu(fileName = "InteractSfxConfigurator",
+        menuName = "Game/BaseGameplay/Configurators/InteractSfxConfigurator")]
+    public class InteractSfxConfigurator : BaseObjectConfigurator
+    {
+        [Header("Sound Settings")] [SerializeField]
+        private SoundData interactSoundData;
+
+        public override void Configure(IBaseObjectRuntime runtime)
+        {
+            // Đăng ký lắng nghe sự kiện tương tác
+            runtime.OnInteract += OnInteract;
+        }
+
+        public override void UnConfigure(IBaseObjectRuntime runtime)
+        {
+            // Hủy đăng ký khi gỡ Configurator để tránh memory leak
+            runtime.OnInteract -= OnInteract;
+        }
+
+        private void OnInteract(IBaseObjectRuntime owner, IObjectInteractable target)
+        {
+            if (owner is not MonoBehaviour ownerMonoBehaviour) return;
+
+            var container = ownerMonoBehaviour.gameObject.GetClosestContainer();
+            var sfxManager = container.Resolve<SfxManager>();
+            sfxManager?.PlayVfx(interactSoundData).Forget();
+        }
+    }
+}
